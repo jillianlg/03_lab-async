0ecd2fb8ae57a34daae49868da44524c8d61f89b
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-12-02 18:21:19 -0800

    adds api fetch for rick and morty characters'

--- fetchers/rickAndMortyFetch.js
+++ fetchers/rickAndMortyFetch.js
@@ -1,2 +1,12 @@
 const fetch = require('node-fetch');
 
+const fetcher = () => {
+    return fetch('https://rickandmortyapi.com/api/character/')
+        .then(res => {
+        return res.json()
+    })
+        .then(json => {
+        console.log(json);
+    });
+}
+fetcher();
